016650cb725e46e083740bb20fad862301c9dfb1
keith <keithnamusasi@gmail.com>
2026-04-02 22:44:49 +0300

    Fix auth routes

--- server/index.js
+++ server/index.js
@@ -14,7 +14,8 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://sangalo-mriy.onrender.com'
+    'https://sangalo-mriy.onrender.com',
+    'https://backend-vgzn.onrender.com'
   ],
   credentials: true
 }));
