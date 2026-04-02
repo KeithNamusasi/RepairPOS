067c054caab0cb9473c3a77c1877e44993bd5979
keith <keithnamusasi@gmail.com>
2026-04-02 10:24:40 +0300

    changes

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -2,7 +2,7 @@ import React, { createContext, useState, useEffect } from 'react';
 
 export const AuthContext = createContext();
 
-const API_URL = 'http://localhost:5001/api/auth';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001/api/auth';
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
